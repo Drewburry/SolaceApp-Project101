b486df2bc208bc9ecd95db5fe0f8f699dafad8c1
Drewburry <persenta3655@gmail.com>
2021-10-05 13:12:28 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App Test</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})

b9eb9774f3f572b0a4f4cd1538a3689907779b13
philasmgwatyu <philamgwatyu@gmail.com>
2021-10-13 09:46:01 +0200

    Philas third commit

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,92 +1,97 @@
 
 import React from 'react'
 import { StyleSheet, Text, View ,Image ,TouchableOpacity} from 'react-native'
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
-const Home = () => {
-    const img = require('../Assets/Images/heart.png');
-    const img1= require('../Assets/Images/Celebb.png');
-    const img2= require('../Assets/Images/star.png');
-    const pro = require('../Assets/Images/profile.png');
-    const dropD = require('../Assets/Images/dropD.png');
-    return (
-        <>
-        <View style={{ backgroundColor:"#FFFFFF",}}>
-        <View style={styles.yellow}>
+const img = require('../Assets/Images/heart.png');
+const img1= require('../Assets/Images/Celebb.png');
+const img2= require('../Assets/Images/star.png');
+const pro = require('../Assets/Images/profile.png');
+const dropD = require('../Assets/Images/dropD.png');
+ const Comments=()=>{
+   return(
+    <View style={styles.person}>
+    <View style={styles.row}>
+ 
+      <View style={styles.prof}>
+        <View style={styles.letter}><Text style={styles.Initials}>L</Text></View>
+    
+        <View><Text style={styles.per}>Landi</Text><Text style={styles.timeSeen}>12 min ago</Text></View>
+        <View></View>
 
-         </View>
-        <View style={styles.head}>
+    </View>
+    </View>
+    <Text style={{paddingLeft:20}}>I love myself .
+        'I encourage everyone to be true to themselves</Text>
+        <View style={{flexDirection:"row",alignSelf:'center'}}>
+        <View style={styles.border} >
+        <Image source={img} style={{width:20, height:20,marginLeft:30}}/>
+        </View>
+        <View style={styles.border}>
+         <Image source={img2} style={{width:20,height:20,marginLeft:30}}/>
+        </View>
+        <View style={styles.border}>
+        <Image source={img1} style={{width:20,height:20,marginLeft:30}}/>
+        </View>
+        </View>
+        <TextInput style={styles.comment} placeholder="Enter comment" />
+  </View>
+   )
+ }
+  const Profile=()=>{
+    return(
+      <>
+      <View style={styles.head}>
         <View style={{}}>
           <Text style={styles.tex}>Your Thoughts</Text>
         </View>
          <View style={styles.Profile}><Image source={pro} style={{width:60,height:60}}/></View>
-        </View>
-        <View style={styles.h2}>
+      </View>
+      <View style={styles.heading2}>
         <Text style={styles.text}>Community Thoughts</Text> 
         <Text style={styles.fav}> favourites </Text>
         <Image source ={dropD} style={{width:15,height:20,marginTop:5}}/>
         </View>
-        <View style={styles.person}>
-          <View style={styles.row}>
-       
-            <View style={styles.prof}>
-              <View style={styles.m}><Text style={styles.Initials}>L</Text></View>
-          
-              <View><Text style={styles.per}>Landi</Text><Text style={styles.personal}>12 min ago</Text></View>
-              <View></View>
+      </>
+    )
+  }
+  
 
-          </View>
-          </View>
-          <Text style={{paddingLeft:20}}>I love myself .
-              'I encourage everyone to be true to themselves</Text>
-              <View style={{flexDirection:"row",alignSelf:'center'}}>
-              <View style={styles.border} >
-              <Image source={img} style={{width:20, height:20,marginLeft:30}}/>
-              </View>
-              <View style={styles.border}>
-               <Image source={img2} style={{width:20,height:20,marginLeft:30}}/>
-              </View>
-              <View style={styles.border}>
-              <Image source={img1} style={{width:20,height:20,marginLeft:30}}/>
-              </View>
-              </View>
-              <View style={styles.comment}><Text>comment...</Text></View>
-        </View>
+const Home = () => {
+   
+    return (
+    
+        <ScrollView  vertical={true} >
+        <View style={{ backgroundColor:"#FFFFFF",height:700}}>
+          {/* profile and home Header */}
+          <View style={{marginLeft:25}}>
+         <Profile/>
+         </View>
+       
+        {/* Comments section */}
+       <View style={{flex:2}}>
+    
+        <Comments/>
+        <Comments/>
+       
+
+       
+        
      
-         <View style={styles.person2}>
-          <View style={styles.row}>
-       
-            <View style={styles.prof}>
-              <View style={styles.m}><Text style={styles.Initials}>M</Text></View>
-          
-              <View><Text style={styles.per}>Mpho</Text><Text style={styles.personal}>10 min ago</Text></View>
-              <View></View>
+     
+     
+     
+       </View>
 
-          </View>
-          
-          </View>
-          <Text style={{paddingLeft:20}}>I love myself .
-              'I encourage everyone to be true to themselves</Text>
-              <View style={{flexDirection:"row",alignSelf:'center',}}>
-              <View style={styles.border2} >
-              <Image source={img} style={{width:20, height:20,marginLeft:30}}/>
-              </View>
-              <View style={styles.border2}>
-               <Image source={img2} style={{width:20,height:20,marginLeft:30}}/>
-              </View>
-              <View style={styles.border2}>
-              <Image source={img1} style={{width:20,height:20,marginLeft:30}}/>
-              </View>
-              </View>
-              <View style={styles.comment}><Text>comment...</Text></View>
-        </View>
-       
-       
-       
-
+     <View>
+   
+       </View>
        <TouchableOpacity style={styles.btnJournal}><Text style={styles.journal}>My journal</Text></TouchableOpacity>
         </View>
         
-         </>
+        
+         </ScrollView>
+
     )
 }
 
@@ -103,12 +108,12 @@ export default Home
         },
       
         tex:{
+          marginTop:5,
           fontSize:25,
           fontFamily:'regular',
           color:'black',
           fontWeight:'900',
           marginLeft:80,
-          paddingTop:50,
           borderBottomWidth:8,
           borderBottomColor:'#F2C66E',
           borderRadius:5
@@ -120,29 +125,28 @@ export default Home
           color:'black',
           fontWeight:'900',
           borderBottomWidth:4,
-          width:180,
+          width:150,
           padding:5,
           paddingLeft:1,
           marginLeft:30
         },
         Profile:{
-         
+          marginTop:5,
           width:50,
           height:50,
           borderRadius:60,
-          margin:50,
-          marginLeft:140
+          marginLeft:70
         },
-        h2:{
+        heading2:{
           flexDirection:'row'
         },
         fav:{
-          paddingLeft:80,
+          paddingLeft:50,
           paddingTop:5
         },
         person:{
           width:'90%',
-          height:'31%',
+          height:'40%',
           margin:'5%',
           alignSelf:'center',
          borderWidth:3,
@@ -151,7 +155,7 @@ export default Home
         },
         person2:{
          width:'90%',
-         height:'30%',
+         height:'36%',
          marginTop:'-0%',
          alignSelf:'center',
        backgroundColor:"#FFFFFF",
@@ -160,36 +164,34 @@ export default Home
        },
         prof:{
         flexDirection:'row',
-       
          borderRadius:100,
          marginLeft:10,
          marginBottom:10,
+
         },
-        m:{
-          width:50,
-         height:50,
+        letter:{
+          width:'12%',
+          height:'75%',
           borderRadius:100,
           borderWidth:1,
           margin:5
      
         },
         Initials:{
-          width:50,
           padding:10,
-          paddingLeft:18,
           fontWeight:"bold",
-          fontSize:18,
+          fontSize:16,
           borderRadius:3,
           alignSelf:'center'
         },
         per:{
-          padding:10,
+          padding:5,
           fontWeight:'bold',
          color:'black'
         },
-        personal:{
-           paddingLeft:10,
-           paddingTop:-20
+        timeSeen:{
+           paddingLeft:5,
+    
         },
         border:{
           border:1,
@@ -209,11 +211,12 @@ export default Home
           },
         comment:{
           width:'70%',
-          height:'14%',
+          height:'20%',
           borderWidth:1,
           alignSelf:'center',
-          marginTop:'3%',
+          marginTop:'5%',
           paddingLeft:10,
+          borderRadius:25,
         }
         ,
         btnJournal:{
@@ -223,7 +226,7 @@ export default Home
           justifyContent:'center',
           alignSelf:"center",
           borderRadius:60,
-         marginTop:-20
+        
         },
         journal:{
           color:'white',
